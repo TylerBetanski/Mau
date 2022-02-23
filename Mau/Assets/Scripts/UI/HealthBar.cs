f4350f36204e7ae5b8f385cf6166181c652c2589
JacobTL11 <JTLibertoski@gmail.com>
2022-02-22 18:13:27 -0600

    Added HealthBar

---
+++ Mau/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    int health;
+    int numOfHearts;
+
+    [SerializeField] Image[] hearts;
+    [SerializeField] Sprite fullHeart;
+    [SerializeField] Sprite emptyHeart;
+
+    private void FixedUpdate() {
+
+        health = GetComponent<PlayerController>().getHealth();
+        numOfHearts = GetComponent<PlayerController>().getMaxHealth();
+
+        if (health > numOfHearts) {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health) {
+                hearts[i].sprite = fullHeart;
+            } else {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else { 
+            hearts[i].enabled = false;
+            }
+        }
+    }
+}
