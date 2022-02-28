537fb8f4c11371cecabf4338424a9ae5b636f301
JacobTL11 <JTLibertoski@gmail.com>
2022-02-28 16:20:07 -0600

    Update to Audio stuff

--- Mau/Assets/Scripts/Audio/BGMTrigger.cs
+++ Mau/Assets/Scripts/Audio/BGMTrigger.cs
@@ -7,16 +7,20 @@ public class BGMTrigger : MonoBehaviour
     [SerializeField] AudioClip BGM;
     [SerializeField] AudioSource BGAudioSource;
 
+    bool changed;
     GameObject player;
     private void Awake()
     {
         player = GameObject.Find("Player 1");
+        changed = false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject == player) {
+
+        if ((other.gameObject == player) && !changed) {
             ChangeBGM();
+            changed = true;
         }
     }
 
