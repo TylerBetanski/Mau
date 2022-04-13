f61c216daba076a2286b61287ff5ca5e415ab0a9
JacobTL11 <JTLibertoski@gmail.com>
2022-04-13 16:49:55 -0500

    Update StatueMoveSound.cs

--- Mau/Assets/Scripts/Audio/StatueMoveSound.cs
+++ Mau/Assets/Scripts/Audio/StatueMoveSound.cs
@@ -4,36 +4,16 @@ using UnityEngine;
 
 public class StatueMoveSound : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-
-        if (collision.gameObject.tag == "Player")
-        {
-            gameObject.GetComponent<AudioSource>().Play();
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            gameObject.GetComponent<AudioSource>().Stop();
-        }
-    }
-
-
-    /*private void FixedUpdate()
+    private void FixedUpdate()
     {
         if (gameObject.GetComponent<Rigidbody2D>().velocity != Vector2.zero)
         {
-            gameObject.GetComponent<AudioSource>().Play();
+            if (!gameObject.GetComponent<AudioSource>().isPlaying)
+                gameObject.GetComponent<AudioSource>().Play();
         }
         else
         {
-            gameObject.GetComponent<AudioSource>().Stop();
+            gameObject.GetComponent<AudioSource>().Pause();
         }
-    }*/
-
-
-
+    }
 }
